c970aad200bc49995971571f69e26dcc55bab90b
ramzdevlab <muhamad.fahmi@outlook.com>
2021-08-11 15:13:27 +0700

    update 1.5

--- javascript/script.js
+++ javascript/script.js
@@ -1,80 +1,87 @@
+function scrolls(range, classes) {
+  if($(this).scrollTop() > range) { 
+    if(classes == "hero"){
+       // HERO SECTION
+        $('.hero .col-md-8 .name').css('margin-top', '3rem');
+        $('.hero .col-md-8 .name').addClass('animate__slideInUp');
+        $('.hero .col-md-8 .profesion').addClass('animate__slideInUp');
+        $('.hero .col-md-8 .sosmed').addClass('animate__slideInUp');
+    }else if(classes == "about") {
+        //ABOUT SECTION
+        $('.hero .col-md-8 .about').addClass('animate__slideInUp');
+    }else if(classes == "skills") {
+        // SKILLS
+        $('.skills').addClass('animate__slideInUp');
+    }else if(classes == "porto") {
+        // PORTOFOLIO
+        $('.portofolio').addClass('animate__slideInUp');
+    }else if(classes == "contact") {
+        // CONTACT
+        $('.contact').addClass('animate__slideInUp');
+    }
+
+
+  } else {
+
+   if(classes == "hero"){
+      // HERO
+      $('.hero .col-md-4 .shadow-img').show();
+      $('.hero .col-md-8 .name').removeClass('animate__slideInUp');
+      $('.hero .col-md-8 .profesion').removeClass('animate__slideInUp');
+      $('.hero .col-md-8 .sosmed').removeClass('animate__slideInUp');
+   }else if(classes == "about") {
+       //ABOUT SECTION
+       $('.hero .col-md-8 .about').removeClass('animate__slideInUp');
+   }else if(classes == "skills") {
+       // SKILLS
+       $('.skills').removeClass('animate__slideInUp');
+   }else if(classes == "porto") {
+       // PORTOFOLIO
+       $('.portofolio').removeClass('animate__slideInUp');
+   }else if(classes == "contact") {
+       // CONTACT
+       $('.contact').removeClass('animate__slideInUp');
+   }
+
+  }
+}
+
 $(document).ready(function() {
-   
     $(window).scroll(function() {
-      if($(this).scrollTop() > 240) { 
-        var x = window.matchMedia("(max-width: 700px)")
-        if(x.matches) {
-            // $('.hero .col-md-4 .shadow-img').addClass('animate__fadeOut');
-            $('.hero .col-md-4 .shadow-img').hide();
-            $('.hero .col-md-8 .name').css('margin-top', '3rem');
-            $('.hero .col-md-8 .name').addClass('animate__slideInUp');
-            $('.hero .col-md-8 .profesion').addClass('animate__slideInUp');
-            $('.hero .col-md-8 .sosmed').addClass('animate__slideInUp');
-        }else {
-            console.log('perangkat desktop')
-        }
-      } else {
-        $('.hero .col-md-4 .shadow-img').show();
-        $('.hero .col-md-8 .name').removeClass('animate__slideInUp');
-        $('.hero .col-md-8 .profesion').removeClass('animate__slideInUp');
-        $('.hero .col-md-8 .sosmed').removeClass('animate__slideInUp');
+      var x = window.matchMedia("(max-width: 700px)");
+
+      if(x.matches) {
+        $('.hero .col-md-4 .shadow-img').hide();
+          scrolls(240, "hero");
+          scrolls(450, "about");
+          scrolls(650, "skills");
+          scrolls(1650, "porto");
+          scrolls(2750, "contact");
+      }else {
+          scrolls(240, "hero");
+          scrolls(450, "about");
+          scrolls(650, "skills");
+          scrolls(1250, "porto");
+          scrolls(1750, "contact");
       }
 
-      if($(this).scrollTop() > 450) { 
-        var x = window.matchMedia("(max-width: 700px)")
-        if(x.matches) {
-            $('.hero .col-md-8 .about').addClass('animate__slideInUp');
-        }else {
-            console.log('perangkat desktop')
-        }
-      } else {
-        $('.hero .col-md-8 .about').removeClass('animate__slideInUp');
-      }
 
-      if($(this).scrollTop() > 650) { 
-        var x = window.matchMedia("(max-width: 700px)")
-        if(x.matches) {
-            $('.skills').addClass('animate__slideInUp');
-        }else {
-            console.log('perangkat desktop')
-        }
-      } else {
-        $('.skills').removeClass('animate__slideInUp');
-      }
-
-      if($(this).scrollTop() > 1650) { 
-        var x = window.matchMedia("(max-width: 700px)")
-        if(x.matches) {
-            $('.portofolio').addClass('animate__slideInUp');
-        }else {
-            console.log('perangkat desktop')
-        }
-      } else {
-        $('.portofolio').removeClass('animate__slideInUp');
-      }
-
-      if($(this).scrollTop() > 2050) { 
-        var x = window.matchMedia("(max-width: 700px)")
-        if(x.matches) {
-            $('.contact').addClass('animate__slideInUp');
-        }else {
-            console.log('perangkat desktop')
-        }
-      } else {
-        $('.contact').removeClass('animate__slideInUp');
-      }
     });
 
     $('.tab1').on('click', function() {
         $('.tab2').removeClass('active');
         $('.tab1').addClass('active');
         $('.software').show();
+        $('.software').addClass('animate__bounceIn');
+        $('.graphic').removeClass('animate__bounceIn');
         $('.graphic').hide();
     });
     
     $('.tab2').on('click', function() {
         $('.tab1').removeClass('active');
         $('.tab2').addClass('active');
+        $('.graphic').addClass('animate__bounceIn');
+        $('.software').removeClass('animate__bounceIn');
         $('.software').hide();
         $('.graphic').show();
     });
