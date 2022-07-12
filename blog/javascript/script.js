4cb519b8b9e452a40f4f43d502e653efba248c37
ramzdevlab <muhamad.fahmi@outlook.com>
2022-07-12 22:02:59 +0700

    rm

--- blog/javascript/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = '../blog/templates/' + $(this).data('include') + '.html'
-      $(this).load(file)
-    })
-  })
-  
